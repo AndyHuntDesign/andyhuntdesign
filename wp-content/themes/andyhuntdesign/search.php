adc81f8f015d51f4763fc18f370fbbd69019d236
Andyh1978 <andt@graphicalliance.co.uk>
2015-10-14 16:16:12 +0100

    Search results

--- wp-content/themes/andyhuntdesign/search.php
+++ wp-content/themes/andyhuntdesign/search.php
@@ -7,29 +7,18 @@
  */
 get_header(); ?>
 <div class="container search-resuts">
-    <div class="row">
-        <div class="span12">
-            <?php if (function_exists('bootstrapwp_breadcrumbs')) {
-                bootstrapwp_breadcrumbs();
-            } ?>
-        </div>
-    </div><!--/.row -->
 
 	<div class="row content">
         <div class="span8">
             <?php if (have_posts()) : ?>
                  <header class="post-title">
-                     <h1><?php printf( __('Search Results for: %s', 'bootstrapwp'),'<span>' . get_search_query() . '</span>'); ?></h1>
+                     <h1><?php printf( __('SEARCH RESULTS FOR: %s', 'bootstrapwp'),'<span>' . get_search_query() . '</span>'); ?></h1>
                  </header>
+                 <div class="portfolio-top-divider"></div>
 
-    		  <?php while (have_posts()) : the_post(); ?>
-
-                <div <?php post_class(); ?>>
-                    <div style="max-width:1180px; margin:0 auto; padding:20px 10px">
-
-
-
-
+    		      <?php while (have_posts()) : the_post(); ?>
+                    <div <?php post_class(); ?>>
+                    <div class="search-result-group">
                     <?php 
                        $post_type = get_post_type($post);
                        switch ($post_type) {
@@ -44,30 +33,27 @@ get_header(); ?>
                                break;
                        }
                     ?>
-
-
                     <a href="<?php the_permalink(); ?>" title="<?php the_title();?>">
                         <h3><?php the_title();?></h3>
                     </a>
-                    <p class="meta">
-                        <?php echo bootstrapwp_posted_on();?>
-                    </p>
-
                     <div class="row">
                         <?php // Post thumbnail conditional display.
                         if ( bootstrapwp_autoset_featured_img() !== false ) : ?>
                             <div class="span6">
-                        <?php else : ?>
-                            <div class="span8 search-entry-result">
-                        <?php endif; ?>
-                                <?php the_excerpt(); ?>
-                            </div>
-                    </div><!-- /.row -->
-                </DIV>
-
+                                <?php else : ?>
+                                <div class="span8 search-entry-result">
+                                    <?php endif; ?>
+                                    <?php the_excerpt(); ?>
+                                    <div class="meta-row"></div>
+                                    <p class="meta">
+                                        <?php echo bootstrapwp_posted_on();?>
+                                    </p>
+                                </div>
+                            </div><!-- /.row -->
+                    </div>
                     <hr/>
                 </div><!-- /.post_class -->
-             <?php endwhile; ?>
+            <?php endwhile; ?>
 
             <?php else : ?>
             	<div class="row content">
@@ -84,9 +70,12 @@ get_header(); ?>
                             <?php get_search_form(); ?>
                         </div><!--/.well -->
              <?php endif;?>
-
              <?php bootstrapwp_content_nav('nav-below'); ?>
+            </div>
         </div>
 
-    <?php get_sidebar(); ?>
+    
+
+
+<div class="portfolio-divider"></div>
     <?php get_footer(); ?>
