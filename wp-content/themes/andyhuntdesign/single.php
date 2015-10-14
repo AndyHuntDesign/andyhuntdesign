fef68d897725efbce2ad1ce0617192e501222874
Andyh1978 <andt@graphicalliance.co.uk>
2015-10-14 17:06:02 +0100

    Blog responsive

--- wp-content/themes/andyhuntdesign/single.php
+++ wp-content/themes/andyhuntdesign/single.php
@@ -34,7 +34,7 @@
             </div>
             <div class="panel">
                 <h1><?php the_title();?></h1>
-                <hr />
+                <hr class="tab-none" />
                 <p><?php the_excerpt(); ?></p>
             </div>
         </div>
