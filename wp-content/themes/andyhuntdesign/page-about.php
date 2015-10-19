cc87f79139343093b5fb411316d213743ce9e8d3
Andy Hunt <andy@andyhuntdesign.com>
2015-10-19 23:27:22 +0100

    About page

--- wp-content/themes/andyhuntdesign/page-about.php
+++ wp-content/themes/andyhuntdesign/page-about.php
@@ -16,65 +16,41 @@
             <h1>MY NAME IS ANDY <span class="mob-none">AND I'M A DIGITAL DESIGNER</span></h1>
             <span class="italic">and</span><span class="sub">&nbsp;here is a little bit about ol' me</span>
         </div>
-        <div class="about-top-divider"></div>
     </div>
 </div>
-
-<!--
-<div class="about-black black black-top">
-    <div class="block-row ">
-        <div class="block-image-left">
-        </div>
-        <div class="block-copy-right">
-            <h2>I wanted to be rock star</h2>
-            <p>After my formative years of busting shapes to the likes of A-Ha and T-Pau, I progressed to being a total grunge head. I still play the banjo today. Rock on!</p>
-        </div>
-    </div>
-</div>
-
-<div class="about-blue">
-    <div class="block-row">
-        <div class="block-copy-left">
-            <h2>I wanted to be a footballer</h2>
-            <p>I had the usual childhood dream of being a professional footballer, but the ambition stopped when the dream was to play for Wolves. I aimed for the stars, so I did</p>
-        </div>
-        <div class="block-image-right"></div>
-    </div>
-</div>
-
-<div class="about-black">
-    <div class="block-image-left"></div>
-    <div class="block-copy-right"></div>
-</div>
--->
-
-
 
 
 <div id="fullpage" style="z-index:1000000">
+    <div class="portfolio-top-divider"></div>
     <div class="section about-black black-top" id="section0">
-        <h2>I wanted to be rock star</h2>
-        <p>After my formative years of busting shapes to the likes of A-Ha and T-Pau, I progressed to being a total grunge head. I still play the banjo today. Rock on!</p>
-        <img src="imgs/fullPage.png" alt="fullPage" />
-    </div>
-    <div class="section about-blue" id="section1">
-        <div class="intro">
-            <h2>I wanted to be a footballer</h2>
-            <p>I had the usual childhood dream of being a professional footballer, but the ambition stopped when the dream was to play for Wolves. I aimed for the stars, so I did</p>
+        <div class="block-row ">
+            <div class="block-image-left">
+            </div>
+            <div class="block-copy-right">
+                <h2>I wanted to be rock star</h2>
+                <p>After my formative years of busting shapes to the likes of A-Ha and T-Pau, I progressed to being a total grunge head. I still play the banjo today. Rock on!</p>
+            </div>
         </div>
     </div>
-    <div class="section about-black" id="section2">
+    <div class="section about-black" id="section1" style="margin-top:-330px; padding-bottom:330px">
         <div class="intro">
-            <h1>No CSS3? No problem!</h1>
+            <div class="block-row ">
+                <div class="block-copy-left">
+                    <h2>I wanted to be a footballer</h2>
+                    <p>I had the usual childhood dream of being a professional footballer, but the ambition stopped when the dream was to play for Wolves. I aimed for the stars, so I did</p>
+                </div>
+                <div class="block-image-right">
+                    <img src="/wp-content/themes/andyhuntdesign/images/about-football.png">
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="section about-black" id="section2" style="margin-top:-330px">
+        <div class="intro">
+            <h1>There is no fold</h1>
             <p>If CSS3 is not available, animations will fall back to jQuery animate.</p>
         </div>
-    </div>
-    <div class="section" id="section3">
-        <div class="intro">
-            <h1>No CSS3? No problem!</h1>
-            <p>If CSS3 is not available, animations will fall back to jQuery animate.</p>
-        </div>
-        <div class="portfolio-divider"></div>
     </div>
 </div>
 
