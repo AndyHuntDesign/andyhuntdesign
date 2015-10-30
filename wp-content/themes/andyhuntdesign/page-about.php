2ca50dc28abad41b22d951acfbcab0cca04f00d8
Andyh1978 <andt@graphicalliance.co.uk>
2015-10-30 16:18:39 +0000

    About Page

--- wp-content/themes/andyhuntdesign/page-about.php
+++ wp-content/themes/andyhuntdesign/page-about.php
@@ -28,8 +28,11 @@
                 <img src="/wp-content/themes/andyhuntdesign/images/about-football.png">
             </div>
             <div class="block-copy-right">
-                <h2>I wanted to be rock star</h2>
+                <i class="rockstar"></i>
+                <h2>I wanted to be a rockstar</h2>
                 <p>After my formative years of busting shapes to the likes of A-Ha and T-Pau, I progressed to being a total grunge head. I still play the banjo today. Rock on!</p>
+                <i class="spotify"></i> 
+                <a href="#">View my Spotify</a>
             </div>
         </div>
     </div>
@@ -37,8 +40,10 @@
         <div class="intro">
             <div class="block-row ">
                 <div class="block-copy-left">
+                    <i class="football"></i>
                     <h2>I wanted to be a footballer</h2>
                     <p>I had the usual childhood dream of being a professional footballer, but the ambition stopped when the dream was to play for Wolves. I aimed for the stars, so I did</p>
+                    <i class="wolves"></i>
                 </div>
                 <div class="block-image-right">
                     <img src="/wp-content/themes/andyhuntdesign/images/about-football.png">
@@ -48,39 +53,41 @@
     </div>
 
         <div class="section about-black black-top" id="section2" style="margin-top:-330px; padding-bottom:330px">
-            <div class="block-row ">
+            <div class="block-row-center">
+                <i class="movie"></i>
+                 <h2>I star in my own movie</h2>
+                 <div class="block-movie">
+                    <img src="/wp-content/themes/andyhuntdesign/images/movie-block.png">
+                 </div>
+                 <p class="movie-copy">I had the usual childhood dream of being a professional footballer, but the ambition stopped when the dream was to play for Wolves. I aimed for the stars, so I did</p>
+                 <i class="youtube"></i>
+            </div>
+        </div>
+
+        <div class="section about-black black-top" id="section2" style="margin-top:-330px; padding-bottom:330px">
+            <div class="block-row-doodle">
                 <div class="block-image-left">
                     <img src="/wp-content/themes/andyhuntdesign/images/about-football.png">
                 </div>
                 <div class="block-copy-right">
+                    <i class="doodle"></i>
                     <h2>Inspired by doodling</h2>
-                    <p>If i’m stuck in a bit of a pickle, I like to doodle ideas down and it helps me out. Not that I’m likely to design a website that’s a bunch of smiley faces with moustaches any time soon</p>
+                    <p>If I’m stuck in a bit of a pickle, I like to doodle ideas down and it helps me out. Not that I’m likely to design a website that’s a bunch of smiley faces with moustaches any time soon</p>
+                    <i class="spotify"></i> 
+                    <a href="#">View my Spotify</a>
                 </div>
             </div>
         </div>
 
-        <div class="section about-black" id="section3" style="margin-top:-330px; padding-bottom:330px">
-            <div class="intro">
-                <div class="block-row ">
-                    <div class="block-copy-left">
-                        <h2>I wanted to be a footballer</h2>
-                        <p>I had the usual childhood dream of being a professional footballer, but the ambition stopped when the dream was to play for Wolves. I aimed for the stars, so I did</p>
-                    </div>
-                    <div class="block-image-right">
-                        <img src="/wp-content/themes/andyhuntdesign/images/about-football.png">
-                    </div>
-                </div>
+         <div class="section about-black black-top" id="section2" style="margin-top:-330px; padding-bottom:330px">
+            <div class="block-row-center">
+                <i class="work"></i>
+                <h2>I work at Graphic Alliance</h2>
+                <img src="/wp-content/themes/andyhuntdesign/images/graphicalliance.png">
+                <p>Like what you see? Hit me up and let's banter. I think that means get in touch</p>
             </div>
         </div>
 
-    
-    <div class="section about-black" id="section5" style="margin-top:-330px;">
-        <div class="intro">
-            <h1>I work at Graphic Alliance in London</h1>
-            <img src="sasas.png">
-            <p>Like what you see? Hiot me up and let's banter. I think that means get in touch</p>
-        </div>
-    </div>
     <div class="section" id="section3" style="pointer-events:none;">
         <div class="intro">
             <div class="portfolio-item-divider" style="position:absolute;top:0"></div>
