65549a10a97aa6e3854c9707cbb6ac87cdcfc780
Andrew Hunt <andy@andyhuntdesign.com>
2016-01-03 18:42:21 +0000

    Blog and portfolio hovers

--- wp-content/themes/andyhuntdesign/page-blog.php
+++ wp-content/themes/andyhuntdesign/page-blog.php
@@ -25,36 +25,40 @@
             <?php while ( have_posts() ) : the_post(); ?>
             <?php global $post; ?>
             <?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array( 5600,1000 ), false, '' );?>
-            <ul> 
-                <li>
-                    <a href="<?php the_permalink() ?>">
-                        <div class="image">
-                            <div class="thumb wow fadeIn" style="background: url(<?php echo $src[0]; ?> ) no-repeat center center!important; background-size:cover!important;"></div>
-                        </div>
-                    </a>
-                </li>
-            </ul>
-            <div class="contents">
-                <div class="pad">
-                    <ul class="wow fadeIn">
-                        <li><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2></li>
-                        <li class="orange"><?php
-                            $categories = get_the_category();
-                            $separator = '   |   ';
-                            $output = '';
-                            if($categories){
-                                foreach($categories as $category) {
-                                    $output .= '<a class="cat" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name. '</a>'.$separator;
-                                    }
-                                    echo trim($output, $separator);
-                            }
-                            ?>
+            <ul class="port-thumb-row">
+                <li class="thumb-row">
+                    <ul> 
+                        <li>
+                            <a href="<?php the_permalink() ?>">
+                                <div class="image">
+                                    <div class="thumb wow fadeIn" style="background: url(<?php echo $src[0]; ?> ) no-repeat center center!important; background-size:cover!important;"></div>
+                                </div>
+                            </a>
                         </li>
-                        <li><?php the_excerpt(); ?></li>
-                        <li><a class="project" href="<?php the_permalink() ?>">View project</a>&nbsp;<img src="<?php echo get_template_directory_uri();?>/images/arrowlink.png" width="12" height="9"></li>
                     </ul>
-                </div>
-            </div>
+                    <div class="contents">
+                        <div class="pad">
+                            <ul class="wow fadeIn">
+                                <li><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2></li>
+                                <li class="orange"><?php
+                                    $categories = get_the_category();
+                                    $separator = '   |   ';
+                                    $output = '';
+                                    if($categories){
+                                        foreach($categories as $category) {
+                                            $output .= '<a class="cat" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '">'.$category->cat_name. '</a>'.$separator;
+                                            }
+                                            echo trim($output, $separator);
+                                    }
+                                    ?>
+                                </li>
+                                <li><?php the_excerpt(); ?></li>
+                                <li><a class="project" href="<?php the_permalink() ?>">View project</a>&nbsp;<img src="<?php echo get_template_directory_uri();?>/images/arrowlink.png" width="12" height="9"></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+            </ul> 
             <?php endwhile;?>
         </div>
     </div>
